6154aa4ddd08721a38f9078fcd457c56b8dfd1e8
Brandon Li <brandon.li@shrgroupllc.com>
2022-08-02 16:03:39 -0400

    go

--- achiveGitLog.groovy
+++ achiveGitLog.groovy
@@ -1,15 +1,9 @@
 node {
   stage('Archive Git Log') {
-<<<<<<< HEAD
-    sh """
-          git log
-       """
-=======
         sh """
              ls -al
              git log > git.log
             """
->>>>>>> be5caf41e40723914cdf37d01f3fb7088f6bedb3
     
     archiveArtifacts artifacts: "git.log", fingerprint: true
   }
